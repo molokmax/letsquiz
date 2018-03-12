bc2174ba7a9682d25ab95759a79d02082c1324c3
Maxim Molokanov <maks.molokanov@gmail.com>
2018-03-13 00:34:14 +0300

    image compress + gulp settings + autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,21 @@
 
 const gulp = require('gulp');
 const del = require('del');
-//const shell = require('gulp-shell');
-//const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
 var exec = require('child_process').exec;
 //var runSequence = require('run-sequence').use(gulp);
 
 const paths = {
-    dist: 'dist',
+    dist: 'D:/develop/denwer/home/localhost/www',
     src: {
-        root: ['*.{js,php,css}', '!gulpfile.js'],
+        root: ['*.{js,php}', '!gulpfile.js'],
+        libs: {
+          smarty: './libs/smarty/**/*', 
+          slick: './slick/**/*', 
+          pnotify: './pnotify/**/*'
+        },
+        css: ['./*.css'],
         app: './app/*',
         fonts: './fonts/*',
         image: './image/*',
@@ -19,39 +25,56 @@ const paths = {
     }
 };
 
-gulp.task('default', ['build', 'watch', 'deploy']);
-
-gulp.task('deploy', function() {
-  setTimeout(function() {
-    exec('deploy.cmd');
-  }, 500);
+gulp.task('default', ['build', 'watch'], function() {
+  //gulp.run('deploy');
 });
+//gulp.task('default', ['build', 'watch', 'deploy']);
+
+// gulp.task('deploy', function() {
+//   setTimeout(function() {
+//     exec('deploy.cmd');
+//   }, 5000);
+// });
 
 gulp.task('clean', function () {
-  del.sync([paths.dist]);
+  del.sync([paths.dist], { force: true });
 });
 
 gulp.task('build', ['clean', 'copy-all']);
 
 gulp.task('watch', function() {
-  setTimeout(function() {
-    gulp.watch(paths.src.root, ['copy-root', 'deploy']);
-    gulp.watch(paths.src.app, ['copy-app', 'deploy']);
-    gulp.watch(paths.src.fonts, ['copy-fonts', 'deploy']);
-    gulp.watch(paths.src.image, ['copy-image', 'deploy']);
-    gulp.watch(paths.src.photo, ['copy-photo', 'deploy']);
-    gulp.watch(paths.src.templates, ['copy-templates', 'deploy']);
-  }, 500);
+  gulp.watch(paths.src.root, ['copy-root']);
+  gulp.watch(paths.src.css, ['copy-css']);
+  gulp.watch(paths.src.app, ['copy-app']);
+  gulp.watch(paths.src.fonts, ['copy-fonts']);
+  gulp.watch(paths.src.image, ['copy-image']);
+  gulp.watch(paths.src.photo, ['copy-photo']);
+  gulp.watch(paths.src.templates, ['copy-templates']);
 });
 
 
 
 // copy block
 
-gulp.task('copy-all', ['copy-root', 'copy-app', 'copy-fonts', 'copy-image', 'copy-photo', 'copy-templates']);
+gulp.task('copy-all', ['copy-libs', 'copy-root', 'copy-app', 'copy-css', 'copy-fonts', 'copy-image', 'copy-photo', 'copy-templates']);
+
+gulp.task('copy-libs', function() {
+  gulp.src(paths.src.libs.smarty)
+    .pipe(gulp.dest(paths.dist + '/libs/smarty/'));
+  gulp.src(paths.src.libs.slick)
+    .pipe(gulp.dest(paths.dist + '/slick/'));
+  gulp.src(paths.src.libs.pnotify)
+    .pipe(gulp.dest(paths.dist + '/pnotify/'));
+});
 
 gulp.task('copy-root', function() {
   gulp.src(paths.src.root)
+    .pipe(gulp.dest(paths.dist + '/'));
+});
+
+gulp.task('copy-css', function() {
+  gulp.src(paths.src.css)
+    .pipe(autoprefixer())
     .pipe(gulp.dest(paths.dist + '/'));
 });
 
@@ -67,11 +90,23 @@ gulp.task('copy-fonts', function() {
 
 gulp.task('copy-image', function() {
   gulp.src(paths.src.image)
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5})
+    ], {
+      verbose: false
+    }))
     .pipe(gulp.dest(paths.dist + '/image/'));
 });
 
 gulp.task('copy-photo', function() {
   gulp.src(paths.src.photo)
+    .pipe(imagemin([
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5})
+    ], {
+      verbose: false
+    }))
     .pipe(gulp.dest(paths.dist + '/photo/'));
 });
 
--- templates/index.tpl
+++ templates/index.tpl
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/2.1.19/css/materialdesignicons.min.css" integrity="sha256-L3NrnCWf0Pw+HvXutUdR0Vlj4pI5pM2s7tijm55+DzU=" crossorigin="anonymous" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 		<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" integrity="sha256-ENFZrbVzylNbgnXx0n3I1g//2WeO47XxoPe0vkp3NC8=" crossorigin="anonymous" />-->
-		<link href="pnotify.custom.min.css" media="all" rel="stylesheet" type="text/css" />
+		<link href="pnotify/pnotify.custom.min.css" media="all" rel="stylesheet" type="text/css" />
 		<link rel="stylesheet" type="text/css" href="slick/slick.css"/>
 		<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
 		<link rel="stylesheet" type="text/css" href="style.css">
@@ -606,7 +606,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
 		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js" integrity="sha256-3blsJd4Hli/7wCQ+bmgXfOdK7p/ZUMtPXY08jmxSSgk=" crossorigin="anonymous"></script>-->
-		<script type="text/javascript" src="pnotify.custom.min.js"></script>
+		<script type="text/javascript" src="pnotify/pnotify.custom.min.js"></script>
 		<script type="text/javascript" src="slick/slick.min.js"></script>
 		<script src="app.js"></script>
 	</body>
