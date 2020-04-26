8584fb09101a5a7527eb72261368f1aa15af8c86
Maxim Molokanov <maks.molokanov@gmail.com>
2020-04-26 23:11:22 +0300

    docker compose setup

--- docker/letsquiz-db-start.cmd
+++
@@ -1,4 +0,0 @@
-xcopy /y ..\database\init.sql .
-xcopy /y ..\database\letsquiz.sql .
-docker build -t letsquiz-db-image -f Dockerfile-db .
-docker run -d -p 33060:3306 -e MYSQL_ROOT_PASSWORD=root --name letsquiz-db letsquiz-db-image
